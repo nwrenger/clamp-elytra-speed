e96fa7b4d77601faf49f8f751cba04be8154ca04
Nils Wrenger <nils@wrenger.net>
2025-11-23 19:55:01 +0100

    :sparkles: Minor refactor && reduced size

--- src/client/java/io/github/nwrenger/elytraspeedcap/mixin/client/LivingEntityElytraSpeedMixin.java
+++ src/client/java/io/github/nwrenger/elytraspeedcap/mixin/client/LivingEntityElytraSpeedMixin.java
@@ -2,9 +2,7 @@ package io.github.nwrenger.elytraspeedcap.mixin.client;
 
 import io.github.nwrenger.elytraspeedcap.ElytraSpeedCap;
 import io.github.nwrenger.elytraspeedcap.ElytraSpeedCapClient;
-import io.github.nwrenger.elytraspeedcap.ElytraSpeedCapConfig;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,38 +13,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityElytraSpeedMixin {
 
-    @Inject(method = "updateFallFlyingMovement", at = @At("RETURN"), cancellable = true)
-    private void elytraspeedcap$capFallFlyingVelocity(Vec3 input,
-            CallbackInfoReturnable<Vec3> cir) {
-        Vec3 result = cir.getReturnValue();
+    @Inject(method = "updateFallFlyingMovement", at = @At(value = "RETURN", target = "Lnet/minecraft/world/entity/LivingEntity;setDeltaMovement(Lnet/minecraft/world/phys/Vec3;)V"), cancellable = true)
+    private void elytraspeedcap$capFallFlyingVelocity(Vec3 input, CallbackInfoReturnable<Vec3> cir) {
+        // Get the original velocity vector
+        Vec3 velocity = cir.getReturnValue();
 
-        LivingEntity self = (LivingEntity) (Object) this;
-
-        if (!(self instanceof Player) || !self.isFallFlying()) {
+        // Cap the velocity vector, returning early if no cap is needed
+        Vec3 capped = ElytraSpeedCap.capElytraVelocity(ElytraSpeedCapClient.maxSpeed, velocity);
+        if (capped == null)
             return;
-        }
-
-        double maxSpeedPerTick = ElytraSpeedCapConfig.intoMaxSpeedPerTick(ElytraSpeedCapClient.maxSpeed);
-        if (maxSpeedPerTick <= 0.0D) {
-            return;
-        }
-
-        double horizontal = Math.sqrt(result.x * result.x + result.z * result.z);
-        if (horizontal <= maxSpeedPerTick) {
-            return;
-        }
-
-        double factor = maxSpeedPerTick / horizontal;
-        Vec3 capped = result.scale(factor);
-
-        ElytraSpeedCap.LOGGER.debug(
-                "[Elytra Speed Cap] Capping HORIZONTAL Elytra velocity for {} from {} to {} (factor={}) oldVel={} newVel={}",
-                self.getName().getString(),
-                horizontal,
-                maxSpeedPerTick,
-                factor,
-                result,
-                capped);
 
         // Send capped vector back
         cir.setReturnValue(capped);
